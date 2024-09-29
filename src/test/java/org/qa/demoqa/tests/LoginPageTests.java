2e51e18abbe6252a7167adb86c408c3056464fa2
Nataliia <pnata1978@gmail.com>
2024-09-29 22:33:33 +0200

    changes-4

---
+++ src/test/java/org/qa/demoqa/tests/LoginPageTests.java
@@ -0,0 +1,15 @@
+package org.qa.demoqa.tests;
+
+import org.qa.demoqa.pages.LoginPage;
+import org.testng.annotations.Test;
+
+public class LoginPageTests extends TestBase{
+
+    @Test
+    public void loginPositiveTest(){
+        new LoginPage(driver)
+
+                .login("Nata", "Pnata1978!");
+    }
+
+}
