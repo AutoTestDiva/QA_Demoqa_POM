634e3e8a288919c638eb3644afc70bb58ef4edf7
Nataliia <pnata1978@gmail.com>
2024-11-10 19:33:58 +0100

    changes-44

--- src/main/java/org/qa/demoqa/pages/elements/UploadPage.java
+++ src/main/java/org/qa/demoqa/pages/elements/UploadPage.java
@@ -1,10 +1,18 @@
 package org.qa.demoqa.pages.elements;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.qa.demoqa.pages.BasePage;
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
+import org.testng.Assert;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.time.Duration;
 
 
 public class UploadPage extends BasePage {
@@ -12,10 +20,38 @@ public class UploadPage extends BasePage {
         super(driver);
     }
 
+    @FindBy(xpath = "//input[@id='uploadFile']")
+    WebElement uploadFile;
     public UploadPage performKeyEventWithRobot() {
         pause(1000);
-        Robot robot = new Robot();
+        ((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 200);");
+        pause(1000);
+        clickWithRectangle(uploadFile,2,3);
 
+        Robot robot = null;
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            throw new RuntimeException(e);
+        }
+        robot.keyPress(KeyEvent.VK_SHIFT);
+        pause(1000);
+        robot.keyPress(KeyEvent.VK_D);
+        robot.keyRelease(KeyEvent.VK_SHIFT);
+        robot.keyPress(KeyEvent.VK_1);
+        robot.keyPress(KeyEvent.VK_PERIOD);
+        robot.keyPress(KeyEvent.VK_T);
+        robot.keyPress(KeyEvent.VK_X);
+        robot.keyPress(KeyEvent.VK_T);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        return this;
+    }
+
+    @FindBy(id = "uploadedFilePath")
+    WebElement uploadedFilePath;
+    public UploadPage assertPath(String path) {
+        ((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 400);");
+        Assert.assertTrue(isTextPresent(uploadedFilePath, path));
         return this;
     }
 }
